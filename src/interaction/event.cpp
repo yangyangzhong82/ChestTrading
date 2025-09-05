ff00419dc02886827a7604f9c360c6b49bd52e1c
Yangyangzhong82 <utorabrl1wkmvg@gmail.com>
2025-09-05 22:26:26 +0800

    feat:支持大箱子

--- src/interaction/event.cpp
+++ src/interaction/event.cpp
@@ -1,12 +1,16 @@
+#include "event.h"
+#include "db/Sqlite3Wrapper.h" // 引入 Sqlite3Wrapper
+#include "form/LockForm.h"
+#include "interaction/chestprotect.h"
 #include "ll/api/event/EventBus.h"
 #include "ll/api/event/player/PlayerInteractBlockEvent.h"
 #include "logger.h"
-#include "interaction/chestprotect.h"
-#include "db/Sqlite3Wrapper.h" // 引入 Sqlite3Wrapper
-#include "mc/platform/UUID.h"   // 引入 UUID
-#include "event.h"
-#include "form/LockForm.h"
+#include "mc/platform/UUID.h" // 引入 UUID
+#include "mc/world/level/BlockSource.h"
+#include "mc\world\level\block\actor\ChestBlockActor.h"
+
 namespace CT {
+
 
 
 void registerEventListener() {
@@ -21,13 +25,27 @@ void registerEventListener() {
             auto  pos               = ev.blockPos();
             bool  isHoldingStick    = (item.getTypeName() == "minecraft:stick");
             std::string player_uuid = player.getUuid().asString();
-
+            auto& region = player.getDimensionBlockSource();
             if (block->getTypeName() != "minecraft:chest") {
                 return; // 只处理箱子
             }
-
             auto [locked, owner_uuid] = isChestLocked(pos, static_cast<int>(dimId));
+            auto* blockActor           = region.getBlockEntity(pos);
+            auto pairedChestBlockActor = static_cast<class ChestBlockActor*>(blockActor)->mLargeChestPaired;
+            // 如果是双箱子，并且另一个箱子存在
+            if (pairedChestBlockActor) {
+                auto pairedChestPos = pairedChestBlockActor->mLargeChestPairedPosition; // 另一个箱子坐标
+                auto [isPairedChestLocked, pairedChestOwnerUuid] = isChestLocked(pairedChestPos, static_cast<int>(dimId));
 
+                // 如果当前箱子未锁定，但配对箱子已锁定，则以配对箱子的锁定状态为准
+                if (!locked && isPairedChestLocked) {
+                    locked = true;
+                    owner_uuid = pairedChestOwnerUuid;
+                }
+                // 如果当前箱子已锁定，但配对箱子未锁定，则以当前箱子的锁定状态为准 (保持不变)
+                // 如果两个都锁定，或者两个都未锁定，则保持原样
+            }
+            
             if (locked) {
                 // 箱子已被锁定
                 if (owner_uuid != player_uuid) {
@@ -60,7 +78,8 @@ void registerEventListener() {
                     return;
                 }
                 // 主人没有手持木棍，允许打开箱子
-            } else {
+            }
+            else {
                 // 箱子未被锁定
                 if (isHoldingStick) {
                     showChestLockForm(player, pos, static_cast<int>(dimId));
